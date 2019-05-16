d42847e3bff6122135713e0da7508e529e42cfe9
xiaojian <xiaojian@iauto.com>
2019-05-16 16:43:45 +0800

    add importantDesign.txt

--- toolclass/EngineFactory/EngineFactory.cpp
+++ toolclass/EngineFactory/EngineFactory.cpp
@@ -1,15 +1,4 @@
-/**
- * Copyright @ 2015 - 2016 Suntec Software(Shanghai) Co., Ltd.
- * All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are NOT permitted except as agreed by
- * Suntec Software(Shanghai) Co., Ltd.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- */
+
 #include "stdafx.h"
 #include <dlfcn.h>
 #include <string>
--- toolclass/EngineFactory/EngineFactory.h
+++ toolclass/EngineFactory/EngineFactory.h
@@ -1,23 +1,4 @@
-/**
- * Copyright @ 2015 - 2016 Suntec Software(Shanghai) Co., Ltd.
- * All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are NOT permitted except as agreed by
- * Suntec Software(Shanghai) Co., Ltd.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- */
 
-/**
- * @file VR_EngineFactory.h
- * @brief interface for Engine Factory.
- *
- *
- * @attention used for C++ only.
- */
 #ifndef VR_ENGINEFACTORY_H
 #define VR_ENGINEFACTORY_H
 
